3e2c700bc4dc1d932021841f04b3e0fbc1fd210a
fortsharif <huseinsharif.96@gmail.com>
2021-08-30 00:36:36 +0100

    removed stringtoList

--- helper/stringToList.js
+++ helper/stringToList.js
@@ -1,11 +1,2 @@
 
 
-module.exports = stringConverter = (data) => {
-    data = data.replace("[", "")
-    data = data.replace("]", "")
-
-    const newData = data.split(":")
-
-    return newData
-
-}
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const net = require('net')
 const WebSocket = require('ws')
-const stringConverter = require('./helper/stringToList')
+
 // Connecting to the TCP-server
 const client = new net.Socket();
 client.connect(8000, '127.0.0.1', () => {
@@ -14,10 +14,10 @@ const websocket = new WebSocket.Server({ port: 8080 })
 websocket.on("connection", ws => {
     client.on('data', (data) => {
         const dataString = data.toString()
-        const dataList = stringConverter(dataString)
 
-        console.log(dataList)
-        ws.send(dataList)
+
+
+        ws.send(dataString)
     })
 
     client.on('close', () => {
